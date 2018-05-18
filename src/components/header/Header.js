648af7e02dd332011f7eef7f6f1512ba4a2a56d7
Randy's Mac <randydavidl78@gmail.com>
2018-05-18 14:23:15 -0400

    converted icon SVGs to components

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+/* custom component imports */
+import {Bell} from '../icons/Icons';
+
 const Header = () => {
   return (
   <header className="header">
     <h1 className="headline--lt header__headline">Your <span className="headline--skinny">App<sup>tm</sup></span></h1>
+    <div className="header__informtion" >
+      <Bell />
+    </div>
   </header>
   );
 }
