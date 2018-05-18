d582dba6cb37ae627361adc7447ad75485456d1a
Randy's Mac <randydavidl78@gmail.com>
2018-05-18 11:26:01 -0400

    added basic sass file layout

---
+++ src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+  <header className="header">
+    <h1 className="headline--lt header__headline">Your <span className="headline--skinny">App<sup>tm</sup></span></h1>
+  </header>
+  );
+}
+
+export default Header
